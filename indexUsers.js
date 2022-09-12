e122f9fd5c0a9b044a040ebd0dc192635ed23607
Guille Zorron <gzorron@xmartlabs.com>
2022-09-12 18:40:26 -0300

    show more details in all users

--- indexUsers.js
+++ indexUsers.js
@@ -3,8 +3,13 @@ const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
 async function main() {
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
